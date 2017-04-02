f1674a7ce131e1fe6a06f14c47d1746e6a01dcc1
Adam Newgas <boris@boristhebrave.com>
2017-04-02 19:37:18 +0100

    Handle inverse cases better.

--- marching_cubes_gen.py
+++ marching_cubes_gen.py
@@ -60,6 +60,37 @@ BASE_CASES = {
     0b10110010: ((8, 11, 0), (0, 11, 5), (5, 11, 6), (0, 5, 1)),
 }
 
+# Normally you can invert the base cases (replace solid with empty and visa versa) to generate more cases
+# But this leads to a problem - such flips in 3d can swap the cube edges from one ambiguous configuration
+# to another.
+#
+#     *-----o          *-----o
+#     |  \  |          | /   |
+#     |   \ |          |/    |
+#     |\    |          |    /|
+#     | \   |          |   / |
+#     o-----*          o-----*
+#
+# This will lead to edges not properly lining up when putting adjacent cells together.
+# To solve this, we specify a few more cases, that are flips of the above, but with a different arrangement of edges
+# See http://users.polytech.unice.fr/~lingrand/MarchingCubes/algo.html
+
+INVERSE_CASES = {
+    255 - 0b00000101: ((3, 2, 8), (8, 2, 10), (8, 10, 1), (8, 1, 0)),
+    255 - 0b01000011: ((6, 8, 3), (6, 9, 8), (6, 5, 9),  (6, 3, 1), (6, 10, 1)),
+    255 - 0b01001010: ((3, 11, 0), (0, 11, 6), (0, 6, 9), (9, 6, 5), (1, 10, 2)),
+    # ?? Docs say this case is needed, but it isn't
+    # 255 - 0b01101001: ((8, 11, 4), (4, 11, 6), (9, 10, 2), (9, 2, 0)),
+    255 - 0b00111010: ((8, 0, 3), (2, 7, 11), (7, 2, 1), (7, 1, 5)),
+    # ?? Docs say this case is needed, but it looks wrong
+    #255 - 0b10100101: ((3, 2, 11), (0, 9, 1), (6, 10, 5), (7, 4, 8)),
+}
+
+# Add INVERSE_CASES to BASE_CASES
+for bits, faces in INVERSE_CASES.items():
+    BASE_CASES[bits] = faces
+
+
 
 # Most opererations are simply vertex permutations
 ROTATE_1 = [
@@ -159,11 +190,13 @@ def pow(op, n):
 cases = {}
 
 # Brute force try all combinations of operations to generate new entries in cases
-for r1 in range(4):
-    for r2 in range(4):
-        for r3 in range(4):
-            for refl in range(2):
-                for invert in (False, True): # Invert swaps solid for empty and visa versa
+# We try inverting last as all, as we want to prefer using an INVERSE_CASE
+# to doing an invert operation, as explained above
+for invert in (False, True):  # Invert swaps solid for empty and visa versa
+    for r1 in range(4):
+        for r2 in range(4):
+            for r3 in range(4):
+                for refl in range(2):
                     op = compose(
                         pow(ROTATE_1, r1),
                         pow(ROTATE_2, r2),
@@ -179,6 +212,45 @@ for r1 in range(4):
                         if new_bits not in cases:
                             new_faces = faces_apply(op, faces, flip)
                             cases[new_bits] = new_faces
+
+
+# Check that we only reference edges that really are between solid and non solid
+def test1():
+    for bits in range(256):
+        verts = set(bits_to_verts(bits))
+        all_edges = set(edge for face in cases[bits] for edge in face)
+        for edge in all_edges:
+            assert sum(v in verts for v in EDGES[edge]) == 1
+
+
+# Check that each case is manifold (except on the cell boundary)
+def test2():
+    from collections import Counter
+    for bits in range(256):
+        edge_pairs = [frozenset([face[i], face[(i+1) % 3]]) for face in cases[bits] for i in range(3)]
+        edge_counts = Counter(edge_pairs)
+        for edge_pair, count in edge_counts.items():
+            # Ok, this edge_pair is used by two faces, forming a continuous surface
+            if count == 2: continue
+            # Find length squared of edge pair
+            def d_midpoint(edge):
+                """Returns the midpoint of the edge, scaled by 2"""
+                v0, v1 = EDGES[edge]
+                v0_pos = VERTICES[v0]
+                v1_pos = VERTICES[v1]
+                return ((x+y) for (x,y) in zip(v0_pos, v1_pos))
+            def length_sq(pos1, pos2):
+                """Returns the square of the length of two positions"""
+                return sum((x-y)**2 for (x,y) in zip(pos1, pos2))
+            edge1, edge2 = edge_pair
+            l2 = length_sq(d_midpoint(edge1), d_midpoint(edge2))
+            if l2 == 2 or l2 == 4:
+                # These are the lengths of the edge_pairs on the boundary
+                continue
+            assert False
+
+test1()
+test2()
 
 # Dump out the results
 # Important - we've made no attempt to deal with ambiguities when inverting
