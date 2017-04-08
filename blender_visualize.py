7843fd2e2b12df0bb9f93aa666ea4d62c085a58c
Adam Newgas <boris@boristhebrave.com>
2017-04-08 23:34:10 +0100

    Add MarchingCube animations

--- blender_visualize.py
+++ blender_visualize.py
@@ -15,7 +15,7 @@ deg_to_rad = 0.017453292523928
 def blender_render(obj_file, output_render):
         # Create Blender script for scene we want to render
         script = ""
-        camera = (10.26851, -4.54741, 5.59207), (63.524*deg_to_rad,0.76*deg_to_rad,65.791*deg_to_rad)
+        camera = (5.95839, -2.64368, 3.245561), (63.524*deg_to_rad,0.76*deg_to_rad,65.791*deg_to_rad)
         # Marshall arguments into blender
         script += "obj_file = {0}\n".format(repr(os.path.abspath(obj_file)))
         script += "output_render = {0}\n".format(repr(os.path.abspath(output_render)))
@@ -27,14 +27,31 @@ def blender_render(obj_file, output_render):
         # Import obj
         bpy.ops.scene.new(type='EMPTY')
         bpy.ops.import_scene.obj("EXEC_DEFAULT", filepath=obj_file)
-        bpy.context.scene.objects.active = bpy.context.scene.objects[0]
+        orig_mesh = bpy.context.scene.objects[0]
 
         # Setup wireframe
+        bpy.context.scene.objects.active = orig_mesh
         bpy.ops.object.duplicate()
         bpy.ops.object.modifier_add(type='WIREFRAME')
-        material = bpy.data.materials.new("black")
-        material.diffuse_color = (0, 0, 0)
+        material = bpy.data.materials.new("red")
+        material.diffuse_color = (1.0, 0, 0)
+        material.use_shadeless = True
         bpy.context.active_object.data.materials.append(material)
+        bpy.context.scene.objects.active.parent = orig_mesh
+
+        # Setup vertices
+        bpy.context.scene.objects.active = orig_mesh
+        bpy.ops.object.particle_system_add()
+        particle_settings = bpy.data.particles["ParticleSettings"]
+        particle_settings.emit_from = 'VERT'
+        particle_settings.show_unborn = True
+        particle_settings.use_emit_random = False
+        particle_settings.render_type = 'OBJECT'
+        particle_settings.physics_type = 'NO'
+        bpy.ops.mesh.primitive_ico_sphere_add(layers=(False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True))
+        psphere = bpy.context.scene.objects.active
+        particle_settings.dupli_object = psphere
+        psphere.data.materials.append(material)
 
         # Setup lighting
         bpy.ops.object.lamp_add(type='POINT', radius=5, location=(5.7, 0.5, 3.5))
@@ -43,6 +60,20 @@ def blender_render(obj_file, output_render):
         my_world = bpy.data.worlds.new("world")
         bpy.context.scene.world = my_world
         bpy.context.scene.world.light_settings.use_environment_light = True
+
+        # Setup animation
+        bpy.context.scene.objects.active = orig_mesh
+        #bpy.context.area.type = 'VIEW_3D'
+        #bpy.context.space_data.context = 'OBJECT'
+        orig_mesh.keyframe_insert('rotation_euler',group="Rot")
+        bpy.context.scene.frame_end = 100
+        bpy.context.scene.frame_current = 101
+        bpy.context.object.rotation_euler[2] = 6.28319
+        orig_mesh.keyframe_insert('rotation_euler',group="Rot")
+        curve = bpy.data.actions[0].fcurves[2]
+        curve.keyframe_points[0].interpolation = 'LINEAR'
+        curve.keyframe_points[1].interpolation = 'LINEAR'
+
 
         # Setup camera and render
         bpy.ops.object.camera_add(location=camera_location,rotation=camera_rotation)
@@ -56,6 +87,10 @@ def blender_render(obj_file, output_render):
 
         # Actually render
         bpy.ops.render.render(write_still=True)
+
+        # Exerimental animation support
+        bpy.data.scenes[1].render.image_settings.file_format = 'H264'
+        bpy.ops.render.render(animation=True)
         """.strip()+"\n\n"
         script +="doit()\n"
         with tempfile.NamedTemporaryFile(delete=False) as f:
